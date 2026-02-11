ee87d78b9f478d5d36236cc6cb5e40cfaf3a3a6a
Ulug'bek <ulugbeek256@gmail.com>
2026-02-11 21:05:13 +1000

    kichik dokon

---
+++ app.js
@@ -0,0 +1,8 @@
+let ism=prompt('ism kiriting:')
+let katta=ism.charAt().toUpperCase()
+let kichik=ism.slice(1).toLowerCase()
+let hammasi=katta+kichik
+console.log(hammasi)
+alert(`Salomm,${hammasi}`)
+
+// console.log(ism)
