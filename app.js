337eda09df5bae8abbb9bc5996b69520b3e0b309
Ulug'bek <ulugbeek256@gmail.com>
2026-02-24 18:47:49 +1000

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let ism=prompt('ism kiriting:')
-let katta=ism.charAt().toUpperCase()
-let kichik=ism.slice(1).toLowerCase()
-let hammasi=katta+kichik
-console.log(hammasi)
-alert(`Salomm,${hammasi}`)
-
-// console.log(ism)
